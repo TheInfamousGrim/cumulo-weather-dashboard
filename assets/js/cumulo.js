b9d5ff43a7523ddb6cff568cff1cd109b1444012
TheInfamousGrim <finchergeorge1@gmail.com>
2022-08-10 20:09:07 +0100

    added functions to style the future card, added dynamic uv index stylings

--- assets/js/cumulo.js
+++ assets/js/cumulo.js
@@ -3,6 +3,7 @@
 /* -------------------------------------------------------------------------- */
 
 // Search for a city section
+const searchCityForm = $('.search-city-form');
 const cityInput = $('.validate');
 const searchBtn = $('.search-city-btn');
 
@@ -16,10 +17,11 @@ const currentWeatherIcon = $('.weather-icon-current');
 const currentTempText = $('#currentTemp');
 const currentWindText = $('#currentWind');
 const currentHumidityText = $('#currentHumidity');
+const currentUVLabel = $('.todays-uv-index-label');
 const currentUVText = $('#currentUV');
 
 // Future weather section
-const forecastContainers = document.querySelectorAll('.future-forecast-container');
+const futureForecastCards = document.querySelectorAll('.future-forecast-card');
 
 // Modal trigger
 document.addEventListener('DOMContentLoaded', () => {
@@ -31,28 +33,82 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-/* -------------------------------- Api call -------------------------------- */
+/* ------------ get the weather conditions for the users location ----------- */
 
 const apiKey = 'c68837c426737828b05c49ecb97695bd';
 
 // Style the current weather card to reflect the current data
 function styleTodaysWeather(icon, temp, wind, humidity, uvIndex) {
     currentWeatherIcon.attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
-    // convert kelvin to floored celsius
+    // convert kelvin to rounded celsius
     const celsiusTemp = Math.round(temp - 273.15);
     currentTempText.text(celsiusTemp);
+
     currentWindText.text(wind);
+
     currentHumidityText.text(humidity);
+
+    // Style the uv label depending on the severity of the uv index
+    if (uvIndex < 3) {
+        currentUVLabel.addClass('uv-index-low');
+    } else if (uvIndex >= 3 && uvIndex < 6) {
+        currentUVLabel.addClass('uv-index-moderate');
+    } else {
+        currentUVLabel.addClass('uv-index-high');
+    }
     currentUVText.text(uvIndex);
 }
 
+// Style the future weather cards
+function styleFutureWeather(days) {
+    // Sets the card counter to zero so it can be incremented
+    let cardCount = 0;
+    // Gets the current day as an integer
+    const currentDayNum = dayjs().day();
+
+    futureForecastCards.forEach((card) => {
+        // get the correct card
+        cardCount += 1;
+
+        // styling the days of each card
+        const cardDayText = $(`.day-${cardCount}-name`);
+        // increments to the next day
+        const cardDayNum = currentDayNum + cardCount;
+        // format the next day
+        const futureDay = dayjs().day(cardDayNum).format('dddd');
+        // style the day text
+        cardDayText.text(futureDay);
+
+        // styling the icon of each card
+        const futureWeatherIconText = $(`.weather-icon-day-${cardCount}`);
+        // set the icon from the api data
+        const futureWeatherIcon = days[cardCount].weather[0].icon;
+        futureWeatherIconText.attr('src', `http://openweathermap.org/img/wn/${futureWeatherIcon}.png`);
+
+        // styling the temp of each card
+        const cardTempText = $(`.future-temp-${cardCount}`);
+        // set and format the temperature from the api data
+        cardTempText.text(`${Math.round(days[cardCount].temp.day - 273.15)}°C`);
+
+        // styling the wind of each card
+        const cardWindText = $(`.future-wind-${cardCount}`);
+        // set and format the wind speed from the api data
+        cardWindText.text(`${days[cardCount].wind_speed}MPH`);
+
+        // styling the humidity of each card
+        const cardHumidityText = $(`.future-humidity-${cardCount}`);
+        // set the humidity of each card
+        cardHumidityText.text(`${days[cardCount].humidity}%`);
+    });
+}
+
 // Get the current weather for a given latitude and longitude
-async function getCurrentWeather(latitude, longitude) {
+async function getWeather(latitude, longitude) {
     const weatherApiUrlCurr = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
     const openWeatherCurrResp = await fetch(weatherApiUrlCurr);
+    // get the current weather data for the users location
     const currentWeatherData = await openWeatherCurrResp.json();
     console.log(currentWeatherData);
-    console.log(currentWeatherData.current.weather[0].icon);
     styleTodaysWeather(
         currentWeatherData.current.weather[0].icon,
         currentWeatherData.current.temp,
@@ -60,6 +116,8 @@ async function getCurrentWeather(latitude, longitude) {
         currentWeatherData.current.humidity,
         currentWeatherData.current.uvi
     );
+    console.log(currentWeatherData.daily);
+    styleFutureWeather(currentWeatherData.daily);
 }
 
 // Get the city name for the current location
@@ -78,35 +136,27 @@ function getLocation() {
         const userLocation = navigator.geolocation.getCurrentPosition((position) => {
             const lat = position.coords.latitude;
             const long = position.coords.longitude;
-            const userCity = getCurrentLoc(lat, long);
-            const userWeatherObj = getCurrentWeather(lat, long);
+            getCurrentLoc(lat, long);
+            getWeather(lat, long);
         });
     } else {
-        console.log('couldnt get users position');
+        console.log("couldn't get users position");
     }
 }
 
 getLocation();
 
-// async function getCurrentCity() {
-//     const geoLocationUrl = await fetch('https://geolocation-db.com/json/ 50ad4a90-fd5e-11ec-b463-1717be8c9ff1');
-//     const userLocation = await geoLocationUrl.json();
-//     console.log(userLocation);
-// }
+// Implement a search city function
 
-// getCurrentCity();
+/* ---------------------- search for your specific city --------------------- */
 
-const weather = {
-    apiKey: 'c68837c426737828b05c49ecb97695bd',
-    fetchWeather: () => {
-        fetch(
-            'https://api.openweather.org/data/2,5/weather?q=denver&units=metric&appid=c68837c426737828b05c49ecb97695bd'
-        )
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    },
-};
+function handleCitySearch(e) {
+    e.preventDefault();
+    console.log(e);
+}
 
-// search for a city
+console.log(searchCityForm);
 
-// when I search for a city then I will produce todays weather and current time
+searchCityForm.addEventListener('submit', handleCitySearch);
+
+// Save search data to the local storage
